4e269bbc81342dde6a63eed8f79c9ad2918dc10a
Sgrpokhriyal2003 <sgrpokhriyal@gmail.com>
2025-07-12 12:01:56 +0530

    added deleet video controller

--- src/controller/video-controller.js
+++ src/controller/video-controller.js
@@ -86,3 +86,27 @@ export const update = async(req, res) => {
         res.status(500).json({message: "something went wrong"});
     }
 }
+
+export const deleteVideo = async(req, res) => {
+    try{
+        const videoId = req.params.id;
+        const video = await Video.findById(videoId)
+        if(!video){
+            return res.status(404).json({message: "video does not exists"})
+        }
+
+        if(video.user_id.toString() !== req.user._id.toString()){
+            return res.status(403).json({error: "Unauthorized!"})
+        }
+
+        await cloudinary.uploader.destroy(video.videoId, {resource_type: "video"})
+        await cloudinary.uploader.destroy(video.thumbnailId)
+
+        await Video.findByIdAndDelete(videoId)
+        res.status(200).json({message: "video deleted successfully!"});
+
+    }
+    catch(err){
+        res.status(500).json({message: 'something went wrong!', error: err.message});
+    }
+}
--- src/routes/video-route.js
+++ src/routes/video-route.js
@@ -2,11 +2,11 @@ import express from 'express'
 const router = express.Router()
 
 import { authMiddleware } from '../middleware/auth.js';
-import { upload, update } from '../controller/video-controller.js';
+import { upload, update, deleteVideo } from '../controller/video-controller.js';
 
 
 router.post("/upload", authMiddleware, upload);
 router.put("/update/:id", authMiddleware, update)
-
+router.delete("/delete/:id", authMiddleware, deleteVideo)
 
 export default router
