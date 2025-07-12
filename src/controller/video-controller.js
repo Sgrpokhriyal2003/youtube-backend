163b7561cb642993f75be36b1f084975a0b13b74
Sgrpokhriyal2003 <sgrpokhriyal@gmail.com>
2025-07-12 11:39:42 +0530

    added video update controller

--- src/controller/video-controller.js
+++ src/controller/video-controller.js
@@ -47,3 +47,42 @@ export const upload = async(req, res) => {
         })
     }
 }
+
+export const update = async(req, res) => {
+    try{
+        const {title, description, category, tags} = req.body;
+        const videoId = req.params.id;
+        let video = await Video.findById(videoId);
+        if(!video){
+            return res.status(404).json({success: false, error: "video not found"})
+        }
+
+        if(video.user_id.toString() !== req.user._id.toString()){
+            return res.status(403).json({error: "unauthorized"})
+        }
+
+        if(req.files && req.files.thumbnailUrl){
+            await cloudinary.uploader.destroy(video.thumbnailId)
+            const thumbnailUpload = await cloudinary.uploader.upload(req.files.thumbnailUrl.tempFilePath, {
+                folder: "thumbnails"
+            })
+
+            video.thumbnailUrl = thumbnailUpload.secure_url
+            video.thumbnailId = thumbnailUpload.public_id
+        }
+
+        video.title = title || video.title
+        video.description = description || video.description
+        video.category = category || video.category
+        video.tags = tags ? tags.split(",") : video.tags
+
+        await video.save()
+        res.status(200).json({
+            success: true,
+            message: "video updated sucessfully!"
+        })
+    }
+    catch(err){
+        res.status(500).json({message: "something went wrong"});
+    }
+}
--- src/routes/video-route.js
+++ src/routes/video-route.js
@@ -2,9 +2,11 @@ import express from 'express'
 const router = express.Router()
 
 import { authMiddleware } from '../middleware/auth.js';
-import { upload } from '../controller/video-controller.js';
+import { upload, update } from '../controller/video-controller.js';
 
 
 router.post("/upload", authMiddleware, upload);
+router.put("/update/:id", authMiddleware, update)
+
 
 export default router
